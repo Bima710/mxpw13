d420fec76695985964ee847b843dc25bae9d8d09
Bima710 <mbima710@gmail.com>
2024-12-13 20:14:40 +0700

    save place

--- week12/firebaseConfig.ts
+++ week12/firebaseConfig.ts
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDfr0ZsR1Tbkqnl5mKy5IVbcEKfPUJxIHo",
-  authDomain: "mobilecrossweek11-5c3c8.firebaseapp.com",
-  databaseURL: "https://mobilecrossweek11-5c3c8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "mobilecrossweek11-5c3c8",
-  storageBucket: "mobilecrossweek11-5c3c8.firebasestorage.app",
-  messagingSenderId: "779584871805",
-  appId: "1:779584871805:web:17ae30310c2813290a0014",
-  measurementId: "G-Y1ZCBGQYF4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
